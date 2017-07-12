e384c34383688a3b421e0e4e71d36c6c662b2aef
Justino Mora <jmora258@gmail.com>
2017-07-12 10:58:51 -0700

    Write tests for PostJob page. Missing tests for button functionality.

--- test/components/postJob-feature.js
+++ test/components/postJob-feature.js
@@ -1,51 +1,84 @@
-const chai = require('chai');
-const assert = chai.assert;
+// const Browser = require('zombie'); // use headless browser 
 
+// Start with a webdriver instance:
+var sw = require('selenium-webdriver');
+var driver = new sw.Builder()
+   .withCapabilities(sw.Capabilities.chrome())
+   .build()
+
+//optional timeout in ms to use with eventually (defaults to 1000)
+  var timeout = 15000;
+//optional interval in ms to use when polling (defaults to 200)
+  var interval = 100;
+
+// // And then...
+  var chai = require('chai');
+  var chaiWebdriver = require('chai-webdriver-promised');
+  chai.use(chaiWebdriver(driver, timeout, interval));
+
+// const assert = chai.assert;
+const expect = chai.expect;
 
 describe('Post Job page', () => {
-  xit('It should display Title field', () => {
+  // before( (done) => {
+  //   driver.get('http://localhost:3000').then(done); 
+  //const browser = new Browser(); // Zombie testing
+  //browser.silent = true; // Zombie testing
 
-  });
-
-  xit('It should display Description field', () => {
-
+  before(() => {
+    driver.get('http://localhost:3000');
+    //browser.visit(`http://localhost:3000/`, done); // for Zombie testing
   }); 
 
-  xit('It should display Address field', () => {
-
+  it('It should display Title field', () => {
+    // expect('#title').dom.to.not.be.visible(); 
+    expect('#title').dom.to.not.be.visible(); 
   });
 
-  xit('It should display Pay field', () => {
+  it('It should display Description field', () => {
+    expect('#description').dom.to.be.visible(); 
+  }); 
 
+  it('It should display Address field', () => {
+    expect('#address').dom.to.be.visible(); 
+  });
+
+  it('It should display Pay field', () => {
+    expect('#pay').dom.to.be.visible(); 
   });
 
   describe('Submit button', () => {
-    xit('Should display Submit button.', () => {
-
+    it('Should display Submit button.', () => {
+      expect('button[type="submit"]').dom.to.be.visible(); // verify this works
     }); 
 
     xit('On click, it should redirect user to the ViewJob page.', () => {
+      let viewJobButton = browser.queryAll(".square")[0]; 
+      // console.log(viewJobButton); 
+      browser.fire(square, 'click', ()=>{
+        // test functionality in here 
+      }); 
 
     }); 
   });
 
   describe('Post Job button', () => {
-    xit('Should display PostJob button', () => {
-
+    it('Should display PostJob button', () => {
+      expect(':button #postJob').dom.to.be.visible();  // ToDo: add this id to button 
     });
 
     xit('On click, it should display PostJob page', () => {
-
+      // TODO
     });
   });
 
   describe('View Job button', () => {
-    xit('Should display View Job button', () => {
-
+    it('Should display View Job button', () => {
+      expect(':button #viewJob').dom.to.be.visible();  // ToDo: add this id to button 
     });
 
     xit('On click, it should reload the ViewPage (current page)', () => {
-
+      // TODO
     });     
   });
 }); 
